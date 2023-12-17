263cd8a1c7a97069d152190b608a2a1f6bd648ab
JavaSoftwareEntwickler <mm.java.software@gmail.com>
2023-12-17 21:38:12 +0100

    Spring security completa con JWT

--- src/main/java/com/marchesinimaxmilliam/security/SecurityApplication.java
+++ src/main/java/com/marchesinimaxmilliam/security/SecurityApplication.java
@@ -2,12 +2,13 @@ package com.marchesinimaxmilliam.security;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties
 public class SecurityApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SecurityApplication.class, args);
 	}
-
 }
---
+++ src/main/java/com/marchesinimaxmilliam/security/token/TokenType.java
@@ -0,0 +1,5 @@
+package com.marchesinimaxmilliam.security.token;
+
+public enum TokenType {
+    BEARER
+}
